3d27010ac88bc03ae9bd6318298ccd570b66b7e8
Aryan Binazir <abinazir@gmail.com>
2026-08-30 09:40:47 -0400

    chore(no-ticket): remove old Hyprland backups

--- stow/arch-linux/other/BACKUP-hypr-config-20260829-165421/hypr/autostart.lua
+++
@@ -1,8 +0,0 @@
--- Extra autostart processes.
--- o.launch_on_start("my-service")
-
--- Ar's desktop startup applications.
-o.launch_on_start("ghostty")
-o.launch_on_start("helium-browser")
-o.launch_on_start("zen-browser")
-o.exec_on_start("sleep 5 && omarchy-launch-webapp https://launchpad.37signals.com")
